7e346fc5addfa94430c9549cbf4440a445c40d85
FairyLd <2950498345@qq.com>
2021-06-07 20:50:58 +0800

    Create 9. Palindrome Number.cpp

---
+++ 1-100/9. Palindrome Number.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return x == rev || x == rev / 10;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    cout<< solution.isPalindrome(121) << endl;
+    return 0;
+}
+
+/*
+给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+
+回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+
+示例 1：
+
+输入：x = 121
+输出：true
+示例 2：
+
+输入：x = -121
+输出：false
+解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+示例 3：
+
+输入：x = 10
+输出：false
+解释：从右向左读, 为 01 。因此它不是一个回文数。
+示例 4：
+
+输入：x = -101
+输出：false
+ 
+
+提示：
+
+-231 <= x <= 231 - 1
+ 
+*/
