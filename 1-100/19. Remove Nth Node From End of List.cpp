e4a88d99ae6dbd313e1ca3ddca4281482fac4fe6
FairyLd <2950498345@qq.com>
2021-06-08 18:12:48 +0800

    Create 19. Remove Nth Node From End of List.cpp

---
+++ 1-100/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* p = new ListNode(0, head);
+        ListNode* first = head;
+        ListNode* second = p;
+        for (int i = 0; i < n; ++i) {
+            first = first->next;
+        }
+        while (first) {
+            first = first->next;
+            second = second->next;
+        }
+        second->next = second->next->next;
+        ListNode* ans = p->next;
+        delete p;
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    
+    return 0;
+}
+
+/*
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+进阶：你能尝试使用一趟扫描实现吗？
+
+
+示例 1：
+
+
+输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
+示例 2：
+
+输入：head = [1], n = 1
+输出：[]
+示例 3：
+
+输入：head = [1,2], n = 1
+输出：[1]
+
+
+提示：
+
+链表中结点的数目为 sz
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+
+*/
