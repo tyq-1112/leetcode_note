492cb29fb875824d42c1e7183b05c7d680fe8a7a
FairyLd <2950498345@qq.com>
2021-06-08 21:47:27 +0800

    Create 21. Merge Two Sorted Lists.cpp

---
+++ 1-100/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) return l2;
+        else if (l2 == nullptr) return l1;
+        else if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
+
+
+int main()
+{
+    Solution solution;
+    return 0;
+}
+
+/*
+
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例 1：
+
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+
+*/
