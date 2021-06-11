b358a8425044785b9eeaa6f57000a44746fffd62
FairyLd <2950498345@qq.com>
2021-06-11 17:38:18 +0800

    Create 35. Search Insert Position.cpp

---
+++ 1-100/35. Search Insert Position.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int> nums, int target) {
+        if(nums.size() == 0 ) return 0;
+        int l = 0 , r = nums.size();
+        while(l < r){
+            int mid = l + r  >> 1;
+            if(nums[mid] >= target) r = mid;
+            else l = mid+1;
+        }
+        return r;
+    }
+};
+
+
+int main()
+{
+    Solution solution;
+    auto t = solution.searchInsert({1,2,3,4,5,6},3);
+    cout << t << endl;
+    return 0;
+}
+
+/*
+
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+你可以假设数组中无重复元素。
+
+示例 1:
+
+输入: [1,3,5,6], 5
+输出: 2
+示例 2:
+
+输入: [1,3,5,6], 2
+输出: 1
+示例 3:
+
+输入: [1,3,5,6], 7
+输出: 4
+示例 4:
+
+输入: [1,3,5,6], 0
+输出: 0
+
+
+*/
