b77ced6254f3092cd4d4a9da0e795ea09a8f6631
FairyLd <2950498345@qq.com>
2021-06-13 00:42:02 +0800

    Create 45. Jump Game II.cpp

---
+++ 1-100/45. Jump Game II.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int> nums){
+        int ans = 0;
+        int end = 0;
+        int maxPos = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            maxPos = max(nums[i] + i, maxPos);
+            if (i == end)
+            {
+                end = maxPos;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    cout << solution.jump({2,3,1,1,4}) << endl;
+    return 0;
+}
+
+/*
+
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+假设你总是可以到达数组的最后一个位置。
+
+ 
+
+示例 1:
+
+输入: [2,3,1,1,4]
+输出: 2
+解释: 跳到最后一个位置的最小跳跃数是 2。
+     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+示例 2:
+
+输入: [2,3,0,1,4]
+输出: 2
+ 
+
+提示:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 10^5
+
+*/
