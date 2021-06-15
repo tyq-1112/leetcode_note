4d9a38d85ae0a5d5e45d969f709dd0665db761d2
FairyLd <2950498345@qq.com>
2021-06-15 17:05:04 +0800

    Create 82. Remove Duplicates from Sorted List II.cpp

---
+++ 1-100/82. Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *dummy = new ListNode(-111);
+        ListNode *p = dummy;
+        while(head){
+            ListNode *q = head;
+            while(q && q->val == head->val) q = q->next; // 双指针
+            //看间距
+            if(q - head == 2 || head->next == nullptr) p = p->next = head;  
+            head = q;
+        }
+        p->next = nullptr;
+        return dummy->next;
+    }
+};
+
+/*
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+
+返回同样按升序排列的结果链表。
+
+ 
+
+示例 1：
+
+
+输入：head = [1,2,3,3,4,4,5]
+输出：[1,2,5]
+示例 2：
+
+
+输入：head = [1,1,1,2,3]
+输出：[2,3]
+ 
+
+提示：
+
+链表中节点数目在范围 [0, 300] 内
+-100 <= Node.val <= 100
+题目数据保证链表已经按升序排列
+
+
+*/
