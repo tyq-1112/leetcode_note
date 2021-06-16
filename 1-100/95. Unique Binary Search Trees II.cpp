2f94323646baed434a326333b23826aa389ae4b3
FairyLd <2950498345@qq.com>
2021-06-16 16:35:59 +0800

    Create 95. Unique Binary Search Trees II.cpp

---
+++ 1-100/95. Unique Binary Search Trees II.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<TreeNode*> dfs(int start, int end) {
+        if (start > end) {
+            return { nullptr };
+        }
+        vector<TreeNode*> allTrees;
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            vector<TreeNode*> leftTrees = dfs(start, i - 1);
+
+            // 获得所有可行的右子树集合
+            vector<TreeNode*> rightTrees = dfs(i + 1, end);
+
+            // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (auto& left : leftTrees) {
+                for (auto& right : rightTrees) {
+                    TreeNode* currTree = new TreeNode(i);
+                    currTree->left = left;
+                    currTree->right = right;
+                    allTrees.emplace_back(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        if (!n) return {};
+        return dfs(1, n);
+    }
+};
+
+
+
+/*
+
+给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。
+
+ 
+
+示例 1：
+
+
+输入：n = 3
+输出：[[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+示例 2：
+
+输入：n = 1
+输出：[[1]]
+ 
+
+提示：
+
+1 <= n <= 8
+
+
+*/
