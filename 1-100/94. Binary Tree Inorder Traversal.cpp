058106c15ee7704842e38745d64aae104618618b
FairyLd <2950498345@qq.com>
2021-06-16 16:35:51 +0800

    Create 94. Binary Tree Inorder Traversal.cpp

---
+++ 1-100/94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+
+    void dfs(TreeNode* root){
+        if(root){
+            dfs(root->left);
+            ans.push_back(root->val);
+            dfs(root->right);
+        }
+    }
+};
+
+
+/*
+
+给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,3,2]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+示例 4：
+
+
+输入：root = [1,2]
+输出：[2,1]
+示例 5：
+
+
+输入：root = [1,null,2]
+输出：[1,2]
+
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+
+*/
