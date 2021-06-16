dba90d196f346413ffa9f457aa22b397a163aec0
FairyLd <2950498345@qq.com>
2021-06-16 16:36:17 +0800

    Create 100. Same Tree.cpp

---
+++ 1-100/100. Same Tree.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
+            return false;
+        } else if (p->val != q->val) {
+            return false;
+        } else {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+    }
+};
+
+
+/*
+
+给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+ 
+
+示例 1：
+
+
+输入：p = [1,2,3], q = [1,2,3]
+输出：true
+示例 2：
+
+
+输入：p = [1,2], q = [1,null,2]
+输出：false
+示例 3：
+
+
+输入：p = [1,2,1], q = [1,1,2]
+输出：false
+ 
+
+提示：
+
+两棵树上的节点数目都在范围 [0, 100] 内
+-10^4 <= Node.val <= 10^4
+
+
+
+*/
