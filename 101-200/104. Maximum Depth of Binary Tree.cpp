192ad3318190676663024d51ad8301a6a91cbaa9
FairyLd <2950498345@qq.com>
2021-06-17 19:45:04 +0800

    Create 104. Maximum Depth of Binary Tree.cpp

---
+++ 101-200/104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return dfs(root);
+    }
+
+    int dfs(TreeNode *root ){
+        if(root == nullptr) return 0;
+        return max(dfs(root->left) , dfs(root->right))+1;
+    }
+};
+
+
+
+
+/*
+
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+
+*/
