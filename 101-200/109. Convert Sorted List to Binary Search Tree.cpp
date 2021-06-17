4be383a9254f7d2735f0651c4a18a1978fd663d5
FairyLd <2950498345@qq.com>
2021-06-17 19:45:25 +0800

    Create 109. Convert Sorted List to Binary Search Tree.cpp

---
+++ 101-200/109. Convert Sorted List to Binary Search Tree.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(!head) return NULL;
+        int n = 0;
+        for(auto p = head ; p ;p=p->next) n++;
+        if(n==1) return new TreeNode(head->val);
+        auto cur = head ;
+        for(int  i = 0 ; i < n/2-1 ; i++) cur = cur->next;
+        auto root = new TreeNode(cur->next->val);
+        root->right = sortedListToBST(cur->next->next);
+        cur->next = NULL;
+        root->left = sortedListToBST(head); 
+        return root;
+    }
+};
+
+
+
+/*
+
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+
+给定的有序链表： [-10, -3, 0, 5, 9],
+
+一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+
+*/
