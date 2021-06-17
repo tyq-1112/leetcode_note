8963f7745b2770ac5cb28ddbb7bd07ad24002c29
FairyLd <2950498345@qq.com>
2021-06-17 19:45:21 +0800

    Create 108. Convert Sorted Array to Binary Search Tree.cpp

---
+++ 101-200/108. Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return build(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode* build(vector<int>& nums, int l, int r) {
+        if (l > r) {
+            return nullptr;
+        }
+
+        int mid = l+r>>1;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = build(nums, l, mid - 1);
+        root->right = build(nums, mid + 1, r);
+        return root;
+    }
+};
+
+
+
+/*
+
+给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+
+高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+
+ 
+
+示例 1：
+
+
+输入：nums = [-10,-3,0,5,9]
+输出：[0,-3,9,-10,null,5]
+解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+
+示例 2：
+
+
+输入：nums = [1,3]
+输出：[3,1]
+解释：[1,3] 和 [3,1] 都是高度平衡二叉搜索树。
+ 
+
+提示：
+
+1 <= nums.length <= 10^4
+-10^4 <= nums[i] <= 10^4
+nums 按 严格递增 顺序排列
+
+
+*/
