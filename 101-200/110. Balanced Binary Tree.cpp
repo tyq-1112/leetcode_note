5174c5e66b258c2d96a8e6df93a096beee1403c3
FairyLd <2950498345@qq.com>
2021-06-17 19:45:29 +0800

    Create 110. Balanced Binary Tree.cpp

---
+++ 101-200/110. Balanced Binary Tree.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans ;
+
+    bool isBalanced(TreeNode* root) {
+        ans = true;
+        dfs(root);
+        return ans;
+    }
+
+    int dfs(TreeNode *root){
+        if(!root) return 0;
+        int lh = dfs(root->left) , rh = dfs(root->right);
+        if(abs(lh-rh) > 1) ans = false;
+        return max(lh,rh)+1;
+    }
+};
+
+
+
+/*
+
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：
+
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,9,20,null,null,15,7]
+输出：true
+示例 2：
+
+
+输入：root = [1,2,2,3,3,null,null,4,4]
+输出：false
+示例 3：
+
+输入：root = []
+输出：true
+ 
+
+提示：
+
+树中的节点数在范围 [0, 5000] 内
+-10^4 <= Node.val <= 10^4
+
+
+
+*/
