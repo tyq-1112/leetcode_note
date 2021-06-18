1e8615dfd389ecdb68ef305a054a24065b994db9
FairyLd <2950498345@qq.com>
2021-06-18 20:37:52 +0800

    Create 118. Pascal's Triangle.cpp

---
+++ 101-200/118. Pascal's Triangle.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret(numRows);
+        for (int i = 0; i < numRows; ++i) {
+            ret[i].resize(i + 1);
+            ret[i][0] = ret[i][i] = 1;
+            for (int j = 1; j < i; ++j) {
+                ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];
+            }
+        }
+        return ret;
+    }
+};
+
+
+
+
+/*
+给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+
+
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 5
+输出:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+
+*/
