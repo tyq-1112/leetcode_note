f9b90dfbf927fe9114029c30bd979fe47e20f304
FairyLd <2950498345@qq.com>
2021-06-18 20:37:39 +0800

    Create 114. Flatten Binary Tree to Linked List.cpp

---
+++ 101-200/114. Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        while(root){
+            auto p = root->left;
+            if(p){
+                while(p->right) p = p->right;
+                p->right = root->right;
+                root->right = root->left;
+                root->left = NULL;
+            }
+            root = root->right;
+        }
+    }
+};
+
+
+
+
+
+/*
+给你二叉树的根结点 root ，请你将它展开为一个单链表：
+
+展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ 
+
+示例 1：
+
+
+输入：root = [1,2,5,3,4,null,6]
+输出：[1,null,2,null,3,null,4,null,5,null,6]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [0]
+输出：[0]
+ 
+
+提示：
+
+树中结点数在范围 [0, 2000] 内
+-100 <= Node.val <= 100
+ 
+
+进阶：你可以使用原地算法（O(1) 额外空间）展开这棵树吗？
+
+
+
+*/
