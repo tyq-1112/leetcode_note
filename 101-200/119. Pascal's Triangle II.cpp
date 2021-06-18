0e2b4e368c9a0d1fbdd4d72dc8c063b0815a557b
FairyLd <2950498345@qq.com>
2021-06-18 20:37:54 +0800

    Create 119. Pascal's Triangle II.cpp

---
+++ 101-200/119. Pascal's Triangle II.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1);
+        row[0] = 1;
+        for (int i = 1; i <= rowIndex; ++i) {
+            for (int j = i; j > 0; --j) {
+                row[j] += row[j - 1];
+            }
+        }
+        return row;
+    }
+};
+
+
+
+
+
+/*
+给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+
+
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 3
+输出: [1,3,3,1]
+进阶：
+
+你可以优化你的算法到 O(k) 空间复杂度吗？
+
+
+*/
