d9fcfac09397a682399ceab7f365a97553e6508d
FairyLd <2950498345@qq.com>
2021-06-19 14:07:25 +0800

    Create 124. Binary Tree Maximum Path Sum.cpp

---
+++ 101-200/124. Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans ;
+    int maxPathSum(TreeNode* root) {
+        //树形dp
+        ans = INT_MIN;
+        dfs(root);
+        return ans;
+    }
+
+    int dfs(TreeNode *root){
+        if(!root) return 0;
+        int left = max(0 , dfs(root->left)) ;   //左子树的最大值
+        int right = max(0 , dfs(root->right));  //右子树的最大值
+        ans = max(ans , root->val + left + right);
+        return root->val + max(left , right);
+    }
+};
+
+
+/*
+
+路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+
+路径和 是路径中各节点值的总和。
+
+给你一个二叉树的根节点 root ，返回其 最大路径和 。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,2,3]
+输出：6
+解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+示例 2：
+
+
+输入：root = [-10,9,20,null,null,15,7]
+输出：42
+解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ 
+
+提示：
+
+树中节点数目范围是 [1, 3 * 10^4]
+-1000 <= Node.val <= 1000
+
+
+*/
