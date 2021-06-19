e6670731dd22d1b9dfb8710d7b646948d8c48606
FairyLd <2950498345@qq.com>
2021-06-19 14:30:22 +0800

    Create 125. Valid Palindrome.cpp

---
+++ 101-200/125. Valid Palindrome.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string sgood;
+        for (char ch: s) {
+            if (isalnum(ch)) {
+                sgood += tolower(ch);
+            }
+        }
+        string sgood_rev(sgood.begin(), sgood.end());
+        return sgood == sgood_rev;
+    }
+};
+
+
+/*
+
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+
+示例 1:
+
+输入: "A man, a plan, a canal: Panama"
+输出: true
+示例 2:
+
+输入: "race a car"
+输出: false
+
+
+*/
