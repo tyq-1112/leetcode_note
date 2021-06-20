1bfda7a98cedf14b8f55a5e89e24d07171fd3c65
FairyLd <2950498345@qq.com>
2021-06-20 16:04:37 +0800

    Create 137. Single Number II.cpp

---
+++ 101-200/137. Single Number II.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; ++i) {
+            int total = 0;
+            for (int num: nums) {
+                total += ((num >> i) & 1);
+            }
+            if (total % 3) {
+                ans += (1 << i);
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+
+给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
+
+ 
+
+示例 1：
+
+输入：nums = [2,2,3,2]
+输出：3
+示例 2：
+
+输入：nums = [0,1,0,1,0,1,99]
+输出：99
+ 
+
+提示：
+
+1 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次
+ 
+
+进阶：你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+*/
