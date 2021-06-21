1654ab9b89f13b2d6992bf329e1721b190bb79eb
FairyLd <2950498345@qq.com>
2021-06-21 12:01:10 +0800

    Create 144. Binary Tree Preorder Traversal.cpp

---
+++ 101-200/144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,74 @@
+#cinlude<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> ans ;
+        //前序： 根 左 右
+        while(st.size() || root!=nullptr){
+            while(root){
+                ans.push_back(root->val);
+                st.push(root);
+                root  = root->left;
+            }
+            root = st.top();
+            st.pop();
+            root = root->right;
+        }
+        return ans;
+    }
+};
+/*
+给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,2,3]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+示例 4：
+
+
+输入：root = [1,2]
+输出：[1,2]
+示例 5：
+
+
+输入：root = [1,null,2]
+输出：[1,2]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+ 
+
+进阶：递归算法很简单，你可以通过迭代算法完成吗？
+
+
+*/
