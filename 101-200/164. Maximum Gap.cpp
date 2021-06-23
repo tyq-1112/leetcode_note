41340a8c1dd62e9b886d9df420d76a8f47194642
FairyLd <2950498345@qq.com>
2021-06-23 17:14:25 +0800

    Create 164. Maximum Gap.cpp

---
+++ 101-200/164. Maximum Gap.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) {
+            return 0;
+        }
+        int exp = 1;
+        vector<int> buf(n);
+        int maxVal = *max_element(nums.begin(), nums.end());
+
+        while (maxVal >= exp) {
+            vector<int> cnt(10);
+            for (int i = 0; i < n; i++) {
+                int digit = (nums[i] / exp) % 10;
+                cnt[digit]++;
+            }
+           
+            for (int i = 1; i < 10; i++) {
+                cnt[i] += cnt[i - 1];
+            } 
+
+            for (int i = n - 1; i >= 0; i--) {
+                int digit = (nums[i] / exp) % 10;
+                buf[cnt[digit] - 1] = nums[i];
+                cnt[digit]--;
+            }
+            copy(buf.begin(), buf.end(), nums.begin());
+            exp *= 10;
+        }
+
+        int ret = 0;
+        for (int i = 1; i < n; i++) {
+            ret = max(ret, nums[i] - nums[i - 1]);
+        }
+        return ret;
+    }
+};
+
+/*
+给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+
+如果数组元素个数小于 2，则返回 0。
+
+示例 1:
+
+输入: [3,6,9,1]
+输出: 3
+解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
+示例 2:
+
+输入: [10]
+输出: 0
+解释: 数组元素个数小于 2，因此返回 0。
+说明:
+
+你可以假设数组中所有元素都是非负整数，且数值在 32 位有符号整数范围内。
+请尝试在线性时间复杂度和空间复杂度的条件下解决此问题。
+
+*/
