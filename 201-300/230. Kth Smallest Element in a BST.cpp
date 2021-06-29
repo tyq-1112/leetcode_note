a79b627799b91216b9a628b9b188a65e4b6fb8f1
FairyLd <2950498345@qq.com>
2021-06-29 18:00:09 +0800

    Create 230. Kth Smallest Element in a BST.cpp

---
+++ 201-300/230. Kth Smallest Element in a BST.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int k, ans;
+
+    int kthSmallest(TreeNode* root, int _k) {
+        k = _k;
+        dfs(root);
+        return ans;
+    }
+
+    //中序遍历
+    bool dfs(TreeNode* root) {
+        if (!root) return false;
+        if (dfs(root->left)) return true;
+        if ( -- k == 0) {
+            ans = root->val;
+            return true;
+        }
+        return dfs(root->right);
+    }
+};
+
+
+
+
+/*
+
+给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,1,4,null,2], k = 1
+输出：1
+示例 2：
+
+
+输入：root = [5,3,6,2,4,null,null,1], k = 3
+输出：3
+ 
+
+ 
+
+提示：
+
+树中的节点数为 n 。
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+ 
+
+进阶：如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化算法？
+
+
+*/
