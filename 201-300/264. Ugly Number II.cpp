5e54a1cc229fa113d54359498c4864837e590643
FairyLd <2950498345@qq.com>
2021-07-03 20:56:20 +0800

    Create 264. Ugly Number II.cpp

---
+++ 201-300/264. Ugly Number II.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n + 1);
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = min(min(num2, num3), num5);
+            if (dp[i] == num2) p2++;
+            
+            if (dp[i] == num3) p3++;
+            
+            if (dp[i] == num5) p5++;
+            
+        }
+        return dp[n];
+    }
+};
+
+
+
+
+/*
+给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+
+丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+
+ 
+
+示例 1：
+
+输入：n = 10
+输出：12
+解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+示例 2：
+
+输入：n = 1
+输出：1
+解释：1 通常被视为丑数。
+ 
+
+提示：
+
+1 <= n <= 1690
+
+*/
