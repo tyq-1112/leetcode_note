e300e21af45d7fa958e07e047aa9e0a29df9cfb4
FairyLd <2950498345@qq.com>
2021-07-03 21:45:14 +0800

    Create 剑指 Offer 06. 从尾到头打印链表.cpp

---
+++ 剑指offer/剑指 Offer 06. 从尾到头打印链表.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> reversePrint(ListNode* head) {
+        if (!head) return {};
+        auto pre = reversePrint(head->next);
+        pre.push_back(head->val);
+        return pre;
+    }
+};
+
+
+/*
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+ 
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+ 
+
+限制：
+
+0 <= 链表长度 <= 10000
+
+
+*/
