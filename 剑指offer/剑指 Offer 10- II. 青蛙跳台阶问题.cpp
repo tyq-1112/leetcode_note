d95e207589ea15418c964b671ac351a57fb131af
FairyLd <2950498345@qq.com>
2021-07-03 21:31:19 +0800

    Create 剑指 Offer 10- II. 青蛙跳台阶问题.cpp

---
+++ 剑指offer/剑指 Offer 10- II. 青蛙跳台阶问题.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numWays(int n) {
+        if(n<2) return 1;
+        if(n==2) return 2;
+        int dp[101] ;
+        memset(dp,0,sizeof dp);
+        dp[1] = 1 ; dp[2] = 2;
+        for(int i=3;i<=n;i++) {
+            dp[i] = dp[i-1]+dp[i-2] ;dp[i] = dp[i]%1000000007;
+        }
+        return dp[n];
+    }
+};
+
+
+/*
+一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+示例 1：
+
+输入：n = 2
+输出：2
+示例 2：
+
+输入：n = 7
+输出：21
+示例 3：
+
+输入：n = 0
+输出：1
+提示：
+
+0 <= n <= 100
+
+*/
