17b30dc24f584b50c2ecfe0cf89174b150869227
FairyLd <2950498345@qq.com>
2021-07-04 10:56:53 +0800

    Create 88. 合并两个有序数组.cpp

---
+++ 数据结构入门/数组/88. 合并两个有序数组.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int  k = n+m-1;
+        int i = m-1 , j = n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] >= nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
+        }
+        while(j>=0) nums1[k--] = nums2[j--];
+    }
+};
+
+
+
+/*
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+输出：[1,2,2,3,5,6]
+示例 2：
+
+输入：nums1 = [1], m = 1, nums2 = [], n = 0
+输出：[1]
+ 
+
+提示：
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[i] <= 109
+
+*/
