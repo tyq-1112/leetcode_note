8290168bb3c1f80d6c66699adf94976fd6877025
FairyLd <2950498345@qq.com>
2021-07-04 14:45:42 +0800

    Create 剑指 Offer 28. 对称的二叉树.cpp

---
+++ 剑指offer/剑指 Offer 28. 对称的二叉树.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        return dfs(root->left , root->right);
+    }
+
+    bool dfs(TreeNode *left , TreeNode *right){
+        if(!left && !right) return true;
+        if(left && !right) return false;
+        if(!left && right) return false;
+        if(left->val != right->val) return false;
+        return dfs(left->left , right->right) && dfs(left->right , right->left);
+    }
+};
+
+
+
+/*
+请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+ 
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+ 
+
+限制：
+
+0 <= 节点个数 <= 1000
+
+*/
