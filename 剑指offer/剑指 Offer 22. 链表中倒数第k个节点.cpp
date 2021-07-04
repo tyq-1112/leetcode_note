2d06f655b99f024636d6971e7fd83406cbfc7e6d
FairyLd <2950498345@qq.com>
2021-07-04 19:37:12 +0800

    Create 剑指 Offer 22. 链表中倒数第k个节点.cpp

---
+++ 剑指offer/剑指 Offer 22. 链表中倒数第k个节点.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        ListNode *fast = head , *low  = head;
+        for(int i = 0 ; i < k ; i++) fast = fast->next;
+        while(fast){
+            low = low->next;
+            fast = fast->next;
+        }
+        return low;
+    }
+};
+
+
+/*
+输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+
+例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+
+ 
+
+示例：
+
+给定一个链表: 1->2->3->4->5, 和 k = 2.
+
+返回链表 4->5.
+
+*/
