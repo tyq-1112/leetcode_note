050a30cf8f5f6410a9dc244928e56784b72b91b5
FairyLd <2950498345@qq.com>
2021-07-04 19:49:01 +0800

    Create 剑指 Offer 17. 打印从1到最大的n位数.cpp

---
+++ 剑指offer/剑指 Offer 17. 打印从1到最大的n位数.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> printNumbers(int n) {
+        int end = pow(10, n) - 1;
+        vector<int> res(end);
+        for(int i = 0; i < end; i++)
+            res[i] = i + 1;
+        return res;
+    }
+};
+
+
+/*
+输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+
+示例 1:
+
+输入: n = 1
+输出: [1,2,3,4,5,6,7,8,9]
+ 
+
+说明：
+
+用返回一个整数列表来代替打印
+n 为正整数
+
+*/
