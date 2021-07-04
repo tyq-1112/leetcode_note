aed014f3f986d28d24bf2476bc717ab94a31887e
FairyLd <2950498345@qq.com>
2021-07-04 20:18:03 +0800

    Create 剑指 Offer 24. 反转链表.cpp

---
+++ 剑指offer/剑指 Offer 24. 反转链表.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head  || !head->next) return head;
+        auto nx = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return nx;
+    }
+};
+
+
+/*
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+ 
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+ 
+
+限制：
+
+0 <= 节点个数 <= 5000
+
+*/
