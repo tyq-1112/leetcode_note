6c634d5e02a7a7c772f1bbfde103825c25ba2c17
FairyLd <2950498345@qq.com>
2021-07-06 22:00:38 +0800

    Create 剑指 Offer 55 - I. 二叉树的深度.cpp

---
+++ 剑指offer/剑指 Offer 55 - I. 二叉树的深度.cpp
@@ -0,0 +1,45 @@
+#cinlude<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        int l = maxDepth(root->right) ;
+        int r = maxDepth(root->left) ;
+        return max(l,r)+1;
+    }
+};
+
+/*
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+例如：
+
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+ 
+
+提示：
+
+节点总数 <= 10000
+
+
+*/
