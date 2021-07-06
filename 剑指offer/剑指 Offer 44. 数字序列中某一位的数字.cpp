1a241d5039ac44da6074e6049957abfc45c9f851
FairyLd <2950498345@qq.com>
2021-07-06 21:22:10 +0800

    Create 剑指 Offer 44. 数字序列中某一位的数字.cpp

---
+++ 剑指offer/剑指 Offer 44. 数字序列中某一位的数字.cpp
@@ -0,0 +1,44 @@
+#cinlude<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findNthDigit(int n) {
+        int digit = 1;   // n所在数字的位数
+        long start = 1;  // 数字范围开始的第一个数
+        long count = 9;  // 占多少位
+        while(n > count){
+            n -= count;
+            digit++;
+            start *= 10;
+            count = digit * start * 9;
+        }
+        long num = start + (n - 1) / digit;
+        return to_string(num)[(n-1)%digit] - '0';
+    }
+};
+
+/*
+数字以0123456789101112131415…的格式序列化到一个字符序列中。在这个序列中，第5位（从下标0开始计数）是5，第13位是1，第19位是4，等等。
+
+请写一个函数，求任意第n位对应的数字。
+
+ 
+
+示例 1：
+
+输入：n = 3
+输出：3
+示例 2：
+
+输入：n = 11
+输出：0
+ 
+
+限制：
+
+0 <= n < 2^31
+
+
+*/
