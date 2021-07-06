447802b5dfd7208b0291fa24b84c67cda54cb3c1
FairyLd <2950498345@qq.com>
2021-07-06 22:23:18 +0800

    Create 剑指 Offer 54. 二叉搜索树的第k大节点.cpp

---
+++ 剑指offer/剑指 Offer 54. 二叉搜索树的第k大节点.cpp
@@ -0,0 +1,66 @@
+#cinlude<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int res , k;
+
+    void dfs(TreeNode* root)               
+    {
+        if (!root) return;
+        
+        dfs(root->right);
+        k--;
+        if(!k) res = root->val;
+        dfs(root->left);
+    }
+
+    int kthLargest(TreeNode* root, int _k) {
+        k = _k;
+        dfs(root);
+        return res;
+    }
+};
+
+/*
+给定一棵二叉搜索树，请找出其中第k大的节点。
+
+ 
+
+示例 1:
+
+输入: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+输出: 4
+示例 2:
+
+输入: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+输出: 4
+ 
+
+限制：
+
+1 ≤ k ≤ 二叉搜索树元素个数
+
+*/
