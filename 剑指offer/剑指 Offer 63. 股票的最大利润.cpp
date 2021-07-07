19a2d1e154c0d5b16014f87c10717193471e4c90
FairyLd <2950498345@qq.com>
2021-07-07 10:07:46 +0800

    Create 剑指 Offer 63. 股票的最大利润.cpp

---
+++ 剑指offer/剑指 Offer 63. 股票的最大利润.cpp
@@ -0,0 +1,43 @@
+#cinlude<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() < 2) return 0;
+
+        int ans = 0;
+        int mx = prices[0] , mi = prices[0];
+        for(int i = 1 ; i<prices.size() ; i++){
+            mx = min(mx , prices[i]);
+            ans = max(ans , prices[i] - mx);
+        }
+        return ans;
+    }
+};
+/*
+
+假设把某股票的价格按照时间先后顺序存储在数组中，请问买卖该股票一次可能获得的最大利润是多少？
+
+ 
+
+示例 1:
+
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+示例 2:
+
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ 
+
+限制：
+
+0 <= 数组长度 <= 10^5
+
+
+*/
