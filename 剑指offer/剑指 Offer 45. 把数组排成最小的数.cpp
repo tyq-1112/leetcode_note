57b09d7468e198cb7cc82fb3b6e0a0ec5b716b85
FairyLd <2950498345@qq.com>
2021-07-08 10:48:08 +0800

    Create 剑指 Offer 45. 把数组排成最小的数.cpp

---
+++ 剑指offer/剑指 Offer 45. 把数组排成最小的数.cpp
@@ -0,0 +1,46 @@
+#cinlude<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+
+    vector<string> num;
+    string res;
+
+    string minNumber(vector<int>& nums) {
+        for(auto &x : nums) num.push_back(to_string(x));
+
+        sort(num.begin(), num.end(), [](string& x, string& y){ return x + y < y + x; });
+
+        for(auto &x : num) res+=x;
+
+        return res;
+    }
+};
+
+
+/*
+输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+
+ 
+
+示例 1:
+
+输入: [10,2]
+输出: "102"
+示例 2:
+
+输入: [3,30,34,5,9]
+输出: "3033459"
+ 
+
+提示:
+
+0 < nums.length <= 100
+说明:
+
+输出结果可能非常大，所以你需要返回一个字符串而不是整数
+拼接起来的数字可能会有前导 0，最后结果不需要去掉前导 0
+
+*/
