a297edddeb63ea72dfa8a1d26e6ca7498ff53222
FairyLd <2950498345@qq.com>
2021-07-08 11:01:42 +0800

    Create 剑指 Offer 57 - II. 和为s的连续正数序列.cpp

---
+++ 剑指offer/剑指 Offer 57 - II. 和为s的连续正数序列.cpp
@@ -0,0 +1,52 @@
+#cinlude<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> findContinuousSequence(int target) {
+        vector<vector<int>>vec;
+        vector<int> res;
+        for (int l = 1, r = 2; l < r;){
+            int sum = (l + r) * (r - l + 1) / 2;
+            if (sum == target) {
+                res.clear();
+                for (int i = l; i <= r; ++i) {
+                    res.push_back(i);
+                }
+                vec.push_back(res);
+                l++;
+            } else if (sum < target) {
+                r++;
+            } else {
+                l++;
+            }
+        }
+        return vec;
+    }
+};
+
+
+/*
+输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+
+序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+
+ 
+
+示例 1：
+
+输入：target = 9
+输出：[[2,3,4],[4,5]]
+示例 2：
+
+输入：target = 15
+输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+ 
+
+限制：
+
+1 <= target <= 10^5
+ 
+
+*/
